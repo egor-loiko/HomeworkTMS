f51697433a510f30bd1db97b702e6e64fbfca4ae
egor.loiko <egor.loiko@softswiss.com>
2023-10-12 16:26:13 +0300

    Homework 11 (Collection) WITH GENERIC

--- src/homeWork11/MyArrayList.java
+++ src/homeWork11/MyArrayList.java
@@ -2,114 +2,129 @@ package homeWork11;
 
 import java.util.Arrays;
 
-public class MyArrayList {
+public class MyArrayList<T> {
 
-    private int[] elements;
+    private T[] elements;
     private int size;
     private static final int DEFAULT_CAPACITY = 10;
 
     public MyArrayList() {
-        this.elements = new int[DEFAULT_CAPACITY];
+        this.elements = (T[]) new Object[DEFAULT_CAPACITY];
     }
 
     public MyArrayList(int initialCapacity) {
         if (initialCapacity >= 0) {
-            this.elements = new int[initialCapacity];
+            this.elements = (T[]) new Object[initialCapacity];
         } else {
             throw new IllegalStateException("Capacity can't be less than 0!");
         }
     }
 
-    public int get(int index) {
-        isIndexExist(index);
-        return elements[index];
+    public T get(int index) {
+        if (isIndexExist(index)) {
+            return elements[index];
+        }
+        return null;
     }
 
     public int size() {
         return size;
     }
 
-    public boolean add(int value) {
+    public void add(T value) {
         if (size == elements.length) {
             elements = increaseCapacity();
         }
         elements[size] = value;
         size++;
-        return true;
     }
 
-    public boolean add(int value, int index) {
-        isIndexExist(index);
-        if (size == elements.length) {
-            elements = increaseCapacity();
+    public void add(T value, int index) {
+        if (isIndexExist(index)) {
+            if (size == elements.length) {
+                elements = increaseCapacity();
+            }
+            T[] temp = (T[]) new Object[size - index];
+            System.arraycopy(elements, index, temp, 0, size - index);
+            elements[index] = value;
+            System.arraycopy(temp, 0, elements, index + 1, temp.length);
+            size++;
+        } else {
+            System.out.println("Data is not added to the List!");
         }
-        int[] temp = new int[size - index];
-        System.arraycopy(elements, index, temp, 0, size - index);
-        elements[index] = value;
-        System.arraycopy(temp, 0, elements, index + 1, temp.length);
-        size++;
-        return true;
     }
 
 
-    private int[] increaseCapacity() {
-        int[] temp = new int[(elements.length + DEFAULT_CAPACITY)];
+    private T[] increaseCapacity() {
+        T[] temp = (T[]) new Object[(elements.length + DEFAULT_CAPACITY)];
         System.arraycopy(elements, 0, temp, 0, elements.length);
         return temp;
     }
 
 
-    public int set(int value, int index) {
-        isIndexExist(index);
-        int temp = elements[index];
-        elements[index] = value;
-        return temp;
+    public T set(T value, int index) {
+        if (isIndexExist(index)) {
+            T temp = elements[index];
+            elements[index] = value;
+            return temp;
+        }
+        return null;
     }
 
     @Override
     public String toString() {
-        int[] temp = new int[size];
+        T[] temp = (T[]) new Object[size];
         System.arraycopy(elements, 0, temp, 0, size);
         return Arrays.toString(temp);
     }
 
 
-    private int isIndexExist(int index) {
+    private boolean isIndexExist(int index) {
         if (index >= size || index < 0) {
-            throw new IndexOutOfBoundsException("Element can't be found! "
-                    + "Number of elements in array = " + size
-                    + ". Total size of array = " + elements.length);
+            System.out.println("Entered Index = " + index + " is out of List bounds. List size = " + size);
+            return false;
         }
-        return index;
+        return true;
     }
 
     public boolean isEmpty() {
         return (size == 0);
     }
 
-    public int remove(int index) {
-        isIndexExist(index);
-        int[] temp = elements;
-        elements = new int[temp.length - 1];
-        int value = temp[index];
-        System.arraycopy(temp, 0, elements, 0, index);
-        System.arraycopy(temp, index + 1, elements, index, temp.length - index - 1);
-        size--;
-        return value;
+    public T remove(int index) {
+        if (isIndexExist(index)) {
+            T[] temp = elements;
+            elements = (T[]) new Object[temp.length - 1];
+            T value = temp[index];
+            System.arraycopy(temp, 0, elements, 0, index);
+            System.arraycopy(temp, index + 1, elements, index, temp.length - index - 1);
+            size--;
+            return value;
+        }
+        return null;
     }
 
     public boolean clear() {
         if (!isEmpty()) {
-            this.elements = new int[DEFAULT_CAPACITY];
+            this.elements = (T[]) new Object[DEFAULT_CAPACITY];
             this.size = 0;
             return true;
         } else {
-            System.err.println("Array is already empty!");
+            System.out.println("Array is already empty!");
             return false;
         }
     }
 
-    public int arrayCapacity() {
+    public boolean isElementExists(T value) {
+        for (int i = 0; i < size; i++) {
+            if (elements[i].equals(value)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public int internalArrayCapacity() {
         return elements.length;
     }
 
